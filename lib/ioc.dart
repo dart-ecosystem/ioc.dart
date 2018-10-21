f447d384d898d36fe95bf4ef920f4ba9b66cb217
copydog <775150@qq.com>
2018-10-21 20:12:01 +0800

    feat: support lazy loading singleton on one and on all

--- lib/ioc.dart
+++ lib/ioc.dart
@@ -8,13 +8,13 @@ library ioc;
 class Ioc {
   static Ioc iocInstance;
 
-  Map<String, dynamic> config = {
-    'singleton': false,
-  };
+  Map<String, dynamic> config = {'singleton': false, 'lazy': false};
 
   Map _bindingMap = new Map();
 
   Map _singletonMap = new Map();
+
+  List _lazyLoadSingletonList = new List();
 
   factory Ioc() {
     if (iocInstance is Ioc) {
@@ -26,21 +26,39 @@ class Ioc {
     return iocInstance;
   }
 
+  static Ioc create() {
+    return new Ioc._();
+  }
+
   Ioc._();
 
-  Ioc bind<T>(dynamic carrier, T initializer(Ioc ioc), {bool singleton}) {
-    if (config['singlton'] == true || singleton == true) {
-      _singletonMap[carrier] = initializer(this);
+  Ioc bind<T>(dynamic carrier, T builder(Ioc ioc), {bool singleton, bool lazy}) {
+    bool shouldUseSingleton = config['singlton'] == true || singleton == true;
+    bool shouldLazyLoadSingleton = config['lazy'] == true || lazy == true;
+
+    if (shouldUseSingleton && shouldLazyLoadSingleton) {
+      _addLazySingleton(carrier, builder);
 
       return this;
     }
 
-    _bindingMap[carrier] = initializer;
+    if (shouldUseSingleton) {
+      _bindSingleton(carrier, builder);
+
+      return this;
+    }
+
+    _bind(carrier, builder);
 
     return this;
   }
 
   T use<T>(dynamic carrier) {
+    if (_isLazyLoadSingleton(carrier)) {
+      _bindSingleton(carrier, _bindingMap[carrier]);
+      _removeLazySingleton(carrier);
+    }
+
     var singleton = _singletonMap[carrier];
 
     if (singleton != null) {
@@ -54,5 +72,27 @@ class Ioc {
     }
 
     return null;
+  }
+
+  bool _isLazyLoadSingleton(dynamic carrier) {
+    return _lazyLoadSingletonList.contains(carrier);
+  }
+
+  void _addLazySingleton<T>(dynamic carrier, T builder) {
+    _bindingMap[carrier] = builder;
+    _lazyLoadSingletonList.add(carrier);
+  }
+
+  void _removeLazySingleton<T>(dynamic carrier) {
+    _bindingMap.remove(carrier);
+    _lazyLoadSingletonList.remove(carrier);
+  }
+
+  void _bindSingleton<T>(dynamic carrier, T builder(Ioc ioc)) {
+    _singletonMap[carrier] = builder(this);
+  }
+
+  void _bind<T>(dynamic carrier, T builder(Ioc ioc)) {
+    _bindingMap[carrier] = builder;
   }
 }
